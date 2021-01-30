0618d5748e2b6b92e0e07b28bdd611954bfe023e
Salih Topcu <>
2021-01-30 20:06:56 +0300

    Added: MovieDetail

---
+++ TMDbMoviesList/Commons/MovieDetailPresentation.swift
@@ -0,0 +1,9 @@
+//
+//  MovieDetailPresentation.swift
+//  TMDbMoviesList
+//
+//  Created by Salih Topcu on 30.01.2021.
+//  Copyright © 2021 Salih Topcu. All rights reserved.
+//
+
+import Foundation
